b50f1754d5e2ef48ed486c093d9a2ee5ad2df3af
Vinn <vinicius.machadoo@outlook.com>
2025-08-15 12:11:38 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000');
+
+    const gatilhoScroll = 150;
+
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > gatilhoScroll) {
+            $('#header-solto').css('top', '0');
+            $('header-fixado').fadeOut();
+        } else {
+            $('#header-solto').css('top', '-100px');
+            $('#header-fixado').fadeIn();
+        }
+    })
+
+    
+
+
+});
